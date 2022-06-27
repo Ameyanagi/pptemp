81facf43ee940092280861ac3ee041fc25397c82
Ameyanagi <77273474+Ameyanagi@users.noreply.github.com>
2022-06-27 11:52:26 -0400

    add tile font size

--- pptemp.py
+++ pptemp.py
@@ -404,7 +404,7 @@ class pptemp(object):
         return file_list, file_sep_list
 
     def add_figure_label_slide(self, dir_path = "./fig/*/", img_path = "*.png", left=0, top=12, width=100, height=88, 
-                               file_regex = re.compile(r".*[_/](.*)\.[a-zA-Z]+"), dir_regex = re.compile(r".*[/_](.*)/"), use_label = True, use_bar = True, label_position = "top"):
+                               file_regex = re.compile(r".*[_/](.*)\.[a-zA-Z]+"), dir_regex = re.compile(r".*[/_](.*)/"), use_label = True, use_bar = True, label_position = "top", title_font_size = 30):
         # Create slides from figures
         dir_list = glob.glob(dir_path)
         dir_list.sort()
@@ -417,7 +417,7 @@ class pptemp(object):
             
             name = re.findall(dir_regex, dir)
             slide_title = name[0]
-            slide  = self.add_content_slide(slide_title, use_bar=use_bar)
+            slide  = self.add_content_slide(slide_title, use_bar=use_bar, font_size=title_font_size)
 
             img_list = self.calc_align_img(file_list, left, top, width, height)
             
